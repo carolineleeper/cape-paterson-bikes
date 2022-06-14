5ea40ef41ecceb2ae169ff33f9ea7088bc902c9e
Caroline Leeper <caroline.leeper@gmail.com>
2022-06-14 22:00:15 +1000

    add header component

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+// import styles from "../styles/Header.module.css";
+import Link from "next/link";
+
+const Header = () => {
+	return (
+		<Link href="/">
+			<a>
+				<h1>Cape Paterson Bike Group</h1>
+			</a>
+		</Link>
+	);
+};
+
+export default Header;
