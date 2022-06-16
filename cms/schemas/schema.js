73156ea915aeec4df177ebd8ce5e7c2d0f39053c
Caroline Leeper <caroline.leeper@gmail.com>
2022-06-16 21:43:46 +1000

    update monthly schedule schema

--- cms/schemas/schema.js
+++ cms/schemas/schema.js
@@ -1,7 +1,8 @@
 import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import ride from "./ride"; // import the file you just made
+import ride from "./ride";
+import monthlySchedule from "./monthlySchedule";
 
 export default createSchema({
 	name: "default",
