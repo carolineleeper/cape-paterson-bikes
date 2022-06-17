07da272a5ca4924c4ded262852d41ad7098f0d6f
Caroline Leeper <caroline.leeper@gmail.com>
2022-06-17 23:36:51 +1000

    output monthly schedule data

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,9 +1,53 @@
-const Calendar = () => {
+import { createClient } from "next-sanity";
+
+const formatMonth = (value) => {
+	const date = new Date(value);
+	const month = date.toLocaleString("default", { month: "short" });
+	const year = date.toLocaleString("default", { year: "numeric" });
+	return month + " " + year;
+};
+
+const formatDate = (value) => {
+	const date = new Date(value);
+	return date.toDateString().slice(0, -5);
+};
+
+const Calendar = ({ monthlySchedule }) => {
 	return (
 		<>
-			<p>Calendar</p>
+			<h1>Calendar</h1>
+			{monthlySchedule.map((schedule) => (
+				<div key={schedule._id}>
+					<h2>{formatMonth(schedule.month)}</h2>
+					<h3>Rides</h3>
+					{schedule.rides.map((ride) => (
+						<>
+							<p>{formatDate(ride.dateTime)}</p>
+							<p>{ride.rideName}</p>
+							<p>{ride.details}</p>
+						</>
+					))}
+				</div>
+			))}
 		</>
 	);
 };
+
+const client = createClient({
+	projectId: "xbdplrfb",
+	dataset: "production",
+	apiVersion: "2022-06-16",
+	useCdn: false,
+});
+
+export async function getStaticProps() {
+	const monthlySchedule = await client.fetch(`*[_type == "monthlySchedule"]`);
+
+	return {
+		props: {
+			monthlySchedule,
+		},
+	};
+}
 
 export default Calendar;
